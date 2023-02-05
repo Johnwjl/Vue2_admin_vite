e2dd01a1c0e4cbe1a4d0467f23b2ffc9b30e6d66
Johnwjl <johnwangjl@gmail.com>
2023-02-05 16:36:13 +0800

    login UI

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
 import { createPinia, PiniaVuePlugin } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './assets/element-variables.scss' // 自定义 element 主题色
 
 Vue.config.productionTip = false // 设置为 false 以阻止 vue 在启动时生成生产提示。
 
--- src/views/TheLogin.vue
+++ src/views/TheLogin.vue
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { ElForm } from 'element-ui/types/form'
   export default {
     name: 'TheLogin',
 
@@ -7,7 +8,23 @@
     props: {},
 
     data() {
-      return {}
+      return {
+        labelPosition: 'left',
+        form: {
+          userName: '',
+          userPassword: ''
+        },
+        rules: {
+          userName: [
+            { required: true, message: '请输入用户名', trigger: 'blur' }
+            // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          userPassword: [
+            { required: true, message: '请输入密码', trigger: 'blur' }
+            // { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ]
+        }
+      }
     },
 
     computed: {},
@@ -18,15 +35,47 @@
 
     //mounted () {},
 
-    methods: {}
+    methods: {
+      login() {
+        ;(this.$refs['form'] as ElForm).validate((valid: boolean) => {
+          if (valid) {
+            alert('submit!')
+          } else {
+            console.log('error submit!!')
+            return false
+          }
+        })
+      }
+    }
   }
 </script>
 <template>
   <div class="login">
     <header>
-      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="50" height="50" />
     </header>
-    <main>123</main>
+    <main>
+      <el-form
+        ref="form"
+        :show-message="false"
+        :model="form"
+        label-width="60px"
+        :label-position="labelPosition"
+        :rules="rules"
+        :hide-required-asterisk="true"
+        style="width: 300px"
+      >
+        <el-form-item label="用户名" prop="userName">
+          <el-input v-model="form.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="userPassword">
+          <el-input v-model="form.userPassword" placeholder="请输入密码" show-password></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="login">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </main>
   </div>
 </template>
 <style scoped lang="less">
@@ -40,6 +89,11 @@
     header {
       width: 125px;
       height: 125px;
+      position: absolute;
+      top: 10%;
+    }
+    main {
+      margin-left: -50px;
     }
   }
 </style>
